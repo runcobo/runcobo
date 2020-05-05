42e58d6cedcb9f3d141e9a1734b6850d1bbc057c
Shootingfly <rocky13670250811@gmail.com>
2020-05-05 14:55:46 +0800

    Support reuse port

--- src/runcobo.cr
+++ src/runcobo.cr
@@ -5,21 +5,35 @@ require "./commands/**"
 require "radix"
 
 module Runcobo
-  # Starts Runcobo App with default host "0.0.0.0" and default port "3000".
+  # Starts Runcobo App with host ("0.0.0.0"), port (3000), reuse_port (false) and handlers (Runcobo::Server.default_handlers).
   #
   # Or read ENV["HOST"] as host, read ENV["PORT"] as port.
   # ```
   # Runcobo.start
   # ```
   #
-  # Starts with custom host and custom port.
+  # Starts with custom host, port, reuse_port and handlers.
   # ```
-  # Runcobo.start(host: "0.0.0.0", port: "5000")
+  # Runcobo.start(
+  #   host: "0.0.0.0",
+  #   port: "5000",
+  #   reuse_port: true,
+  #   handlers: [
+  #     Runcobo::RouteHandler.new,
+  #     Runcobo::RouteNotFoundHandler.new,
+  #   ]
+  # )
   # ```
-  def self.start(*, host : String? = ENV["HOST"]? || "0.0.0.0", port : Int32 = (ENV["PORT"]? || 3000).to_i)
+  def self.start(*,
+                 host : String = ENV["HOST"]? || "0.0.0.0",
+                 port : Int32 = (ENV["PORT"]? || 3000).to_i,
+                 reuse_port : Bool = false,
+                 handlers : Array(HTTP::Handler) = Runcobo::Server.default_handlers)
     server = Runcobo::Server.new
     server.host = host
     server.port = port
+    server.reuse_port = reuse_port
+    server.handlers = handlers
     server.listen
   end
 end
--- src/runcobo/server.cr
+++ src/runcobo/server.cr
@@ -6,19 +6,21 @@ module Runcobo
     property host : String = ENV["HOST"]? || "0.0.0.0"
     # Port, defaults to ENV["PORT"] or 3000.
     property port : Int32 = (ENV["PORT"]? || 3000).to_i
+    # Reuse Port, defaults to false.
+    property reuse_port : Bool = false
     # Handlers, defaults to `.default_handlers`.
     property handlers : Array(HTTP::Handler) = default_handlers
     # Server instance.
     getter instance : HTTP::Server
 
     def initialize
-      @instance = HTTP::Server.new(@handlers)
+      @instance = HTTP::Server.new(handlers)
     end
 
     # Creates a TCPServer listening on `host`:`port`.
     def listen
       Runcobo::Log.info { "listen on #{host}:#{port}" }
-      instance.bind_tcp(host: host, port: port)
+      instance.bind_tcp(host: host, port: port, reuse_port: reuse_port)
       Signal::INT.trap { close }
       Signal::TERM.trap { close }
       instance.listen
