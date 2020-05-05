6cfccdbd579a370471be30405adf33ca9a0d57f7
Shootingfly <rocky13670250811@gmail.com>
2020-05-05 18:49:18 +0800

    Support https

--- src/runcobo.cr
+++ src/runcobo.cr
@@ -12,7 +12,7 @@ module Runcobo
   # Runcobo.start
   # ```
   #
-  # Starts with custom host, port, reuse_port and handlers.
+  # Starts with custom host, port, reuse_port, handlers, cert and key.
   # ```
   # Runcobo.start(
   #   host: "0.0.0.0",
@@ -21,19 +21,25 @@ module Runcobo
   #   handlers: [
   #     Runcobo::RouteHandler.new,
   #     Runcobo::RouteNotFoundHandler.new,
-  #   ]
+  #   ],
+  #   cert: "localhost.crt",
+  #   key: "localhost.key"
   # )
   # ```
   def self.start(*,
                  host : String = ENV["HOST"]? || "0.0.0.0",
                  port : Int32 = (ENV["PORT"]? || 3000).to_i,
                  reuse_port : Bool = false,
-                 handlers : Array(HTTP::Handler) = Runcobo::Server.default_handlers)
+                 handlers : Array(HTTP::Handler) = Runcobo::Server.default_handlers,
+                 cert : String? = ENV["CERT"]?,
+                 key : String? = ENV["KEY"]?)
     server = Runcobo::Server.new
     server.host = host
     server.port = port
     server.reuse_port = reuse_port
     server.handlers = handlers
+    server.cert = cert
+    server.key = key
     server.listen
   end
 end
--- src/runcobo/server.cr
+++ src/runcobo/server.cr
@@ -1,15 +1,20 @@
 require "http/server"
+require "openssl"
 
 module Runcobo
   class Server
-    # Host, defaults to ENV["HOST"] or "0.0.0.0".
-    property host : String = ENV["HOST"]? || "0.0.0.0"
-    # Port, defaults to ENV["PORT"] or 3000.
-    property port : Int32 = (ENV["PORT"]? || 3000).to_i
+    # Host, defaults to "0.0.0.0".
+    property host : String = "0.0.0.0"
+    # Port, defaults to 3000.
+    property port : Int32 = 3000
     # Reuse Port, defaults to false.
     property reuse_port : Bool = false
     # Handlers, defaults to `.default_handlers`.
     property handlers : Array(HTTP::Handler) = default_handlers
+    # SSL Certificate file.
+    property cert : String? = nil
+    # SSL key file.
+    property key : String? = nil
     # Server instance.
     getter instance : HTTP::Server
 
@@ -20,7 +25,11 @@ module Runcobo
     # Creates a TCPServer listening on `host`:`port`.
     def listen
       Runcobo::Log.info { "listen on #{host}:#{port}" }
-      instance.bind_tcp(host: host, port: port, reuse_port: reuse_port)
+      if (certificate_chain = cert) && (private_key = key)
+        instance.bind_tls(host: host, port: port, context: generate_tls_context(certificate_chain, private_key), reuse_port: reuse_port)
+      else
+        instance.bind_tcp(host: host, port: port, reuse_port: reuse_port)
+      end
       Signal::INT.trap { close }
       Signal::TERM.trap { close }
       instance.listen
@@ -45,6 +54,14 @@ module Runcobo
         Runcobo::RouteHandler.new,
         Runcobo::RouteNotFoundHandler.new,
       ] of HTTP::Handler
+    end
+
+    # Generate TLS context with certificate_chain and private_key
+    def generate_tls_context(certificate_chain : String, private_key : String) : OpenSSL::SSL::Context::Server
+      context = OpenSSL::SSL::Context::Server.new
+      context.certificate_chain = certificate_chain
+      context.private_key = private_key
+      context
     end
   end
 end
